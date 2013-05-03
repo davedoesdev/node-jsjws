f3ac08c492ebed0f07e6a23aa7e13d44dd84844c
David Halls <dahalls@gmail.com>
2013-05-03 10:06:11 +0100

    Adapter functions for new version of js-rsa-pem

--- make_index.sh
+++ make_index.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cat > index.js <<EOF
-var navigator = {
-  appName: "Netscape"
-};
-function alert(s)
-{
-  throw new Error(s);
-}
-var jsonParse = JSON.parse;
-EOF
-cat {jsjws/{jsbn,jsbn2,base64,base64x-1.1,sha512,rsa,rsa2,rsasign-1.2,asn1hex-1.0,jws-1.1},js-rsa-pem/rsa-pem}.js >> index.js
+cat node.js {jsjws/{jsbn,jsbn2,base64,base64x-1.1,sha512,rsa,rsa2,rsasign-1.2,asn1hex-1.0,jws-1.1},js-rsa-pem/rsa-pem}.js adapt.js > index.js
 cat >> index.js <<EOF
 exports.RSAKey = RSAKey;
 exports.JWS = JWS;
