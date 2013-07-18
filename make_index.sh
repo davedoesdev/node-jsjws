2f642e387db5ac439575d8081212d20c3de6cea8
David Halls <dahalls@gmail.com>
2013-07-19 00:04:02 +0100

    Add functions required for PSS

--- make_index.sh
+++ make_index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cat node.js {jsjws/{jsbn,jsbn2,base64,base64x-1.1,sha512,rsa,rsa2,rsasign-1.2,asn1hex-1.0,jws-1.1},js-rsa-pem/rsa-pem}.js adapt.js > index.js
+cat node.js {jsjws/{jsbn,jsbn2,base64,base64x-1.1,sha256,sha512,rsa,rsa2,rsasign-1.2,asn1hex-1.0,jws-1.1},js-rsa-pem/rsa-pem}.js adapt.js > index.js
 cat >> index.js <<EOF
 exports.RSAKey = RSAKey;
 exports.JWS = JWS;
--- node.js
+++ node.js
@@ -1,8 +1,29 @@
 var navigator = {
   appName: "Netscape"
 };
+
 function alert(s)
 {
   throw new Error(s);
 }
+
 var jsonParse = JSON.parse;
+var crypto = require('crypto');
+
+function SecureRandom()
+{
+    "use strict";
+    return undefined;
+}
+
+SecureRandom.prototype.nextBytes = function (ba)
+{
+    "use strict";
+
+    var rb = crypto.randomBytes(ba.length), i;
+
+    for (i = 0; i < ba.length; i += 1)
+    {
+        ba[i] = rb[i];
+    }
+};
