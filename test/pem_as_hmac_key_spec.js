ada98a7486c4795ef3f6e3f9a81ed470a1ae99f2
David Halls <dahalls@gmail.com>
2015-03-10 23:05:51 +0000

    Patch jsjws to do constant time comparison

--- test/pem_as_hmac_key_spec.js
+++ test/pem_as_hmac_key_spec.js
@@ -1,4 +1,13 @@
-
+/*global expect: false,
+         it: false,
+         describe: false,
+         pub_pem: false,
+         jsjws: false,
+         before: false,
+         payload: false,
+         pub_keys: false */
+/*jslint node: true */
+"use strict";
 
 describe('pem-as-hmac-key', function ()
 {
@@ -12,13 +21,13 @@ describe('pem-as-hmac-key', function ()
         token = new jsjws.JWT().generateJWTByKey({ alg: 'HS256' }, payload, expires, pub_pem);
     });
 
-    it('should verify token using public PEM string as public key when no algorithm is specified', function ()
+    it('should verify token using public PEM string as public key when no allowed algorithm is specified', function ()
     {
         // verify token using public PEM string as public key
         new jsjws.JWT().verifyJWTByKey(token, pub_pem);
     });
 
-    it('should fail to verify token using public PEM string as public key when algorithm is specified', function ()
+    it('should fail to verify token using public PEM string as public key when allowed algorithm is specified', function ()
     {
         // specify expected algorithm this time
         expect(function ()
@@ -34,12 +43,12 @@ describe('pem-as-hmac-key', function ()
     {
         expect(function ()
         {
-            new jsjws.JWT().verifyJWTByKey(token, pub_keys['RS256'].fast);
+            new jsjws.JWT().verifyJWTByKey(token, pub_keys.RS256.fast);
         }).to.throw();
 
         expect(function ()
         {
-            new jsjws.JWT().verifyJWTByKey(token, pub_keys['RS256'].slow);
+            new jsjws.JWT().verifyJWTByKey(token, pub_keys.RS256.slow);
         }).to.throw();
     });
 });
